c01b6cbf7e13390b875c5ae026e64c4fc5e134d4
HAL101010 <3218497724@qq.com>
2023-05-13 20:39:16 +0800

    new file:   components/player2.js

---
+++ components/player2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet,Text,View} from 'react-native';
+
+const player = () => {
+    return(
+        <View>
+            <Text>hello world</Text>
+        </View>
+    );
+}
