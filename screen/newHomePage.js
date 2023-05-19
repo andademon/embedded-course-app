62cebdb719b46c63c7971955cc9259abf16bbeae
HAL101010 <3218497724@qq.com>
2023-05-19 20:24:40 +0800

    modified:   package.json 	modified:   screen/newHomePage.js 	modified:   yarn.lock

--- screen/newHomePage.js
+++ screen/newHomePage.js
@@ -1,51 +1,75 @@
 import React, { useState,useEffect } from 'react';
 import { StyleSheet, View, Button, Text, Alert, TouchableOpacity, TouchableHighlight } from 'react-native';
-import { FontAwesomeIcon } from 'react-native-fontawesome';
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+
 
 export default function NewHomePage(){
     return (
         <View style = {styles.container}>
-            <View style = {{height:50,width:500,marginLeft:100,marginTop:30}}><Text style = {{textAlign:'left',marginLeft:80,fontSize:20}}>icon</Text></View>
+            <View style = {{height:50,marginTop:30}}></View>
             {/*分割界线*/}
             <View style = {styles.mainTouchableArea}>
-                <TouchableOpacity>   
-                    <Text style = {{textAlign:'left',marginLeft:60,fontSize:20,marginTop:10}}>Profiles</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View style = {styles.mainTouchableArea}>
-                <TouchableOpacity style = {{flex:1}}>
-                        <Text style = {{textAlign:'left',marginLeft:60,fontSize:20}}>Live</Text>
-                </TouchableOpacity>
+                <TouchableHighlight style = {{flex:1}} onPress={() => {}} activeOpacity={0.6} underlayColor="#DDDDDD">
+                    <View style = {{flex:1,flexDirection:'row',justifyContent:'center'}}>
+                        <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}><AntDesign name="profile" size={32} color="black" /></View>
+                        <View style = {{flex:4,justifyContent:'center'}}><Text style = {styles.mainText}>Profiles</Text></View>
+                    </View>
+                </TouchableHighlight>
             </View>
 
             <View style = {styles.mainTouchableArea}>
                 <TouchableHighlight style = {{flex:1}} onPress={() => {}} activeOpacity={0.6} underlayColor="#DDDDDD">
-                    <Text style = {{textAlign:'left',marginLeft:60,fontSize:20}}>Weather</Text>
+                    <View style = {{flex:1,flexDirection:'row',justifyContent:'center'}}>
+                        <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}><MaterialIcons name="live-tv" size={32} color="black" /></View>
+                        <View style = {{flex:4,justifyContent:'center'}}><Text style = {styles.mainText}>Live</Text></View>
+                    </View>
                 </TouchableHighlight>
             </View>
 
-            {/* <FontAwesomeIcon icon="mug-saucer" /> */}
+            <View style = {styles.mainTouchableArea}>
+                <TouchableHighlight style = {{flex:1}} onPress={() => {}} activeOpacity={0.6} underlayColor="#DDDDDD">
+                    <View style = {{flex:1,flexDirection:'row',justifyContent:'center'}}>
+                        <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}><MaterialCommunityIcons name="weather-partly-cloudy" size={32} color="black" /></View>
+                        <View style = {{flex:4,justifyContent:'center'}}><Text style = {styles.mainText}>Weather</Text></View>
+                    </View>
+                </TouchableHighlight>
+            </View>
 
             {/*分割界线*/}
             <View style = {styles.otherTouchableArea}>
-                <TouchableHighlight style = {{flex:1,justifyContent:'center'}} onPress={() => {}} activeOpacity={0.6} underlayColor="#DDDDDD" >
-                    <Text style = {styles.mainText}>Logs</Text>
+                <TouchableHighlight style = {{flex:1}} onPress={() => {}} activeOpacity={0.6} underlayColor="#DDDDDD" >
+                    <View style = {{flex:1,flexDirection:'row',justifyContent:'center'}}>
+                        <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}><FontAwesome5 name="clipboard-list" size={32} color="black" /></View>
+                        <View style = {{flex:4,justifyContent:'center'}}><Text style = {styles.mainText}>Logs</Text></View>
+                    </View>
                 </TouchableHighlight>
             </View>
             <View style = {styles.otherTouchableArea}>
-                <TouchableHighlight style = {{flex:1,justifyContent:'center'}} onPress={() => {}} activeOpacity={0.6} underlayColor="#DDDDDD" >
-                    <Text style = {styles.mainText}>Settings</Text>
+                <TouchableHighlight style = {{flex:1}} onPress={() => {}} activeOpacity={0.6} underlayColor="#DDDDDD" >
+                    <View style = {{flex:1,flexDirection:'row',justifyContent:'center'}}>
+                        <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}><MaterialIcons name="settings" size={32} color="black" /></View>
+                        <View style = {{flex:4,justifyContent:'center'}}><Text style = {styles.mainText}>Settings</Text></View>
+                    </View>
                 </TouchableHighlight>
             </View>
             <View style = {styles.otherTouchableArea}>
-                <TouchableHighlight style = {{flex:1,justifyContent:'center'}} onPress={() => {}} activeOpacity={0.6} underlayColor="#DDDDDD" >
-                    <Text style = {styles.mainText}>Support</Text>
+                <TouchableHighlight style = {{flex:1}} onPress={() => {}} activeOpacity={0.6} underlayColor="#DDDDDD" >
+                    <View style = {{flex:1,flexDirection:'row',justifyContent:'center'}}>
+                        <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}><MaterialIcons name="help-center" size={32} color="black" /></View>
+                        <View style = {{flex:4,justifyContent:'center'}}><Text style = {styles.mainText}>Help</Text></View>
+                    </View>
                 </TouchableHighlight>
             </View>
             <View style = {styles.otherTouchableArea}>
-                <TouchableHighlight style = {{flex:1,justifyContent:'center'}} onPress={() => {}} activeOpacity={0.6} underlayColor="#DDDDDD" >
-                    <Text style = {styles.mainText}>About</Text>
+                <TouchableHighlight style = {{flex:1}} onPress={() => {}} activeOpacity={0.6} underlayColor="#DDDDDD" >
+                    <View style = {{flex:1,flexDirection:'row',justifyContent:'center'}}>
+                        <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}><FontAwesome name="exclamation-circle" size={32} color="black" /></View>
+                        <View style = {{flex:4,justifyContent:'center'}}><Text style = {styles.mainText}>About</Text></View>
+                    </View>
                 </TouchableHighlight>
             </View>
 
@@ -72,13 +96,12 @@ const styles = StyleSheet.create({
         elevation:2
     },
     otherTouchableArea:{
-        height:40,
+        height:60,
         width:350,
         marginBottom:10,
     },
     mainText:{
         textAlign:'left',
         fontSize:20,
-        marginLeft:60
     }
 });
