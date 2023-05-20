f84588ad86475a2aa60c0825cfe506ccf819bbc8
HAL101010 <3218497724@qq.com>
2023-05-20 19:59:08 +0800

    modified:   screen/Live.js 	modified:   screen/Weather.js

--- screen/Live.js
+++ screen/Live.js
@@ -16,22 +16,59 @@ const getData = async (key) => {
 
 export default function Live(){
     const [videoURL,setVideoURL] = useState("");
+    const [serverURL,setServerURL] = useState("");
 
     useEffect(() => {
         async function getVideoURL(){
             let videoURI = await getData("VideoURL");
             setVideoURL(videoURI);
         }
+        async function getServerURL(){
+            let serverURL = await getData("ServerURL");
+            setServerURL(serverURL);
+        }
         getVideoURL();
+        getServerURL();
         setTimeout(() => {
             console.log(videoURL);
         },1000)
     },[])
 
+    async function turnLeft(){
+        try {
+            let response = await fetch(serverURL + "/left",{
+                method:"POST",
+            });
+            let data = await response.json().then((data) => {
+                console.log(data);
+            });
+        } catch (error) {
+            Alert.alert("ERROR",error.toString(),[{text:"OK",style: "default"}]);
+        }
+    }
+
+    async function turnRight(){
+        try {
+            let response = await fetch(serverURL + "/right",{
+                method:"POST",
+            });
+            let data = await response.json().then((data) => {
+                console.log(data);
+            });
+        } catch (error) {
+            Alert.alert("ERROR",error.toString(),[{text:"OK",style: "default"}]);
+        }
+    }
+
     return (
         <View style = {styles.container}>
             <View style = {{flex:1,marginTop:10}}>
                 <MyVideo videoURI={videoURL} />
+                <View style = {{height:60,flexDirection: 'row',marginTop:10,marginBottom:10,alignItems:'center',justifyContent:'center'}}>
+                <View style = {{flex:1,paddingRight:10}}><Button title='left' onPress={turnLeft}></Button></View>
+                <View style = {{flex:1,paddingLeft:10}}><Button title='right' onPress={turnRight}></Button></View>
+            </View>
+            {/* <View><Text>111222</Text></View> */}
             </View>
         </View>
     );
